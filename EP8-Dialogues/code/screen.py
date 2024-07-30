2f9e7de7053f529e676471b1c7b81450e03c46ee
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-07-30 14:14:15 +0200

    add a logo for the project

--- EP8-Dialogues/code/screen.py
+++ EP8-Dialogues/code/screen.py
@@ -12,6 +12,7 @@ class Screen:
         self.imagescreen: pygame.Surface = pygame.display.get_surface()
         self.display: pygame.display = pygame.display.set_mode((1280, 720))
         pygame.display.set_caption("Pokémon")
+        pygame.display.set_icon(pygame.image.load("../../assets/app/logo_projet_pokemon.png"))
         self.clock: pygame.time.Clock = pygame.time.Clock()
         self.framerate: int = 240
         self.deltatime: float = 0.0
